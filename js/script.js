f3fbfe5a0530501573536e7a0d4b922ea755cb5a
GiOrGI175 <nozadzegiorgi1011@gmail.com>
2024-05-03 03:12:44 +0400

    last commit
    all done

--- js/script.js
+++ js/script.js
@@ -6,3 +6,7 @@ btn.addEventListener('click', function () {
   navUl.classList.toggle('nav_bar_transform');
   btn.classList.toggle('btnopacity');
 });
+
+let li_btn = document.querySelector('.li_background');
+
+let btn_a = document.querySelector('.btn_a');
